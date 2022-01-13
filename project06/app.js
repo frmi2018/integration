6477aba161f1753f0396a42317007382dee7d902
frmi <frmi2018@gmail.com>
2022-01-13 18:29:12 +0100

    P6-3 Récupérer les réponses

--- project06/app.js
+++ project06/app.js
@@ -1,1 +1,15 @@
+const form = document.querySelector('.form-quizz');
+let tableauResultats = [];
 
+form.addEventListener('submit', (e) => {
+  // empécher le rafraichissement de la page
+  e.preventDefault();
+  // récupérer les réponses
+  for (i = 1; i < 6; i++) {
+    tableauResultats.push(
+      document.querySelector(`input[name="q${i}"]:checked`).value
+    );
+  }
+  console.log(tableauResultats);
+  tableauResultats = [];
+});
