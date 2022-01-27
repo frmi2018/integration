134bb0b060dc9f17a3c5de1af80a1c91aaf3c8fd
frmi <frmi2018@gmail.com>
2022-01-27 12:33:15 +0100

    Appel API PokeAPI

--- project09/app.js
+++ project09/app.js
@@ -1,5 +1,15 @@
 const searchInput = document.querySelector('.recherche-poke input');
 
+function fetchPokemonBase() {
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .then((reponse) => reponse.json())
+    .then((allPoke) => {
+      console.log(allPoke);
+    });
+}
+
+fetchPokemonBase();
+
 // Animation input
 
 searchInput.addEventListener('input', function (e) {
