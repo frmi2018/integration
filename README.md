7e38e46190854b2a4d408e1e48212976b5b72780
frmi <frmi2018@gmail.com>
2022-02-09 18:34:50 +0100

    html

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 [Projet 02 - Casse-briques](http://frmi.free.fr/perso/project02/breakout.html) version originale  
 [Projet 02b - Casse-briques](http://frmi.free.fr/perso/project02/exercice2.html) version modifiée  
 [Projet 03 - Citations](http://frmi.free.fr/perso/project03/citations.html) addEventListener click / manipulation du DOM avec JS  
-[Projet 04 - Météo](http://frmi.free.fr/perso/project04/meteo.html) Bootstrap / API openweathermap / XMLHttpRequest  
+🌤️[Projet 04 - Météo](http://frmi.free.fr/perso/project04/meteo.html) Bootstrap / API openweathermap / XMLHttpRequest  
 [Projet 04b - Météo 2](http://frmi.free.fr/perso/project04/meteo2.html) avec géolocation  
 [Projet 08 - Météo 3](http://frmi.free.fr/perso/project08/index.html) loader / API / fetch / objet Date / import & export module en JS / géolocation  
 [Projet 05 - Le juste prix](http://frmi.free.fr/perso/project05/justePrix.html) addEventListener submit & keyup / manipulation du DOM avec JS  
@@ -24,17 +24,18 @@
 
 ## Tests :
 
-[Calculatrice](http://frmi.free.fr/perso/tests/calculatrice.html) /
+🧮[Calculatrice](http://frmi.free.fr/perso/tests/calculatrice.html) /
 [Theme](http://frmi.free.fr/perso/tests/theme.html) /
-[Timer](http://frmi.free.fr/perso/tests/timer.html) /
+⌛[Timer](http://frmi.free.fr/perso/tests/timer.html) /
 [Bitcoin](http://frmi.free.fr/perso/tests/bitcoin.html) /
 [Calcul IMC](http://frmi.free.fr/perso/tests/calculerIMC.html)  
 [Spoiler](http://frmi.free.fr/perso/tests/spoiler.html) /
 [Modal](http://frmi.free.fr/perso/tests/modal-js/index.html) /
 [btn-go-up-page](http://frmi.free.fr/perso/tests/bgup.html) /
-[Page recette du site Marmiton](http://frmi.free.fr/perso/tests/marmiton/index.html) /
-[Page du site Tripadvisor](http://frmi.free.fr/perso/tests/tripadvisor/index.html)  
+🥞[Une page du site Marmiton](http://frmi.free.fr/perso/tests/marmiton/index.html) /
+🏠[Une page du site Tripadvisor](http://frmi.free.fr/perso/tests/tripadvisor/index.html)  
 [Smooth Scroll](http://frmi.free.fr/perso/tests/smooth-scroll.html) /
-[Créateur de cookie](http://frmi.free.fr/perso/tests/cookies/index.html) /
+🍪[Créateur de cookie](http://frmi.free.fr/perso/tests/cookies/index.html) /
 [ToDoList](http://frmi.free.fr/perso/tests/ToDoJS/index.html) /
-[Pomodoro](http://frmi.free.fr/perso/tests/Pomodoro/index.html)
+🍅[Pomodoro](http://frmi.free.fr/perso/tests/Pomodoro/index.html) /
+[Formulaire](http://frmi.free.fr/perso/tests/ValidationForm/index.html)
--- tests/Pomodoro/app.js
+++ tests/Pomodoro/app.js
@@ -6,53 +6,66 @@ const btnReset = document.querySelector(".b3");
 const cycles = document.querySelector("h2");
 
 let checkInterval = false;
-let tempsInitial = 1800;
-let tempsDeRepos = 300;
+let tempsInitial = 3;
+let tempsDeRepos = 3;
 let pause = false;
 let nbDeCycles = 0;
 cycles.innerText = `nombre de cycles ${nbDeCycles}`;
 
-affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${
-  tempsInitial % 60 < 10 ? `0${tempsInitial % 60}` : tempsInitial % 60
-}`;
-
-affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${
-  tempsDeRepos % 60 < 10 ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60
-}`;
+afficheTemps(tempsInitial, tempsDeRepos);
 
 btnGo.addEventListener("click", () => {
   if (checkInterval === false) {
     checkInterval = true;
 
     tempsInitial--;
-    affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${
-      tempsInitial % 60 < 10 ? `0${tempsInitial % 60}` : tempsInitial % 60
-    }`;
+    afficheTemps(tempsInitial, tempsDeRepos);
     let timer = setInterval(() => {
-      if (tempsInitial > 0) {
+      if (pause === false && tempsInitial > 0) {
         tempsInitial--;
-        affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${
-          tempsInitial % 60 < 10 ? `0${tempsInitial % 60}` : tempsInitial % 60
-        }`;
-      } else if (tempsDeRepos === 0 && tempsInitial === 0) {
-        tempsInitial = 1800;
-        tempsDeRepos = 300;
-        affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${
-          tempsInitial % 60 < 10 ? `0${tempsInitial % 60}` : tempsInitial % 60
-        }`;
-        affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${
-          tempsDeRepos % 60 < 10 ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60
-        }`;
+        afficheTemps(tempsInitial, tempsDeRepos);
+      } else if (pause === false && tempsDeRepos === 0 && tempsInitial === 0) {
+        tempsInitial = 3;
+        tempsDeRepos = 3;
+        afficheTemps(tempsInitial, tempsDeRepos);
         nbDeCycles++;
         cycles.innerText = `nombre de cycles ${nbDeCycles}`;
-      } else if (tempsInitial === 0) {
+      } else if (pause === false && tempsInitial === 0) {
         tempsDeRepos--;
-        affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${
-          tempsDeRepos % 60 < 10 ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60
-        }`;
+        afficheTemps(tempsInitial, tempsDeRepos);
       }
     }, 1000);
+
+    // Reset
+    btnReset.addEventListener("click", () => {
+      clearInterval(timer);
+      checkInterval = false;
+      tempsInitial = 3;
+      tempsDeRepos = 3;
+      nbDeCycles = 0;
+      afficheTemps(tempsInitial, tempsDeRepos);
+      cycles.innerText = `nombre de cycles ${nbDeCycles}`;
+    });
   } else {
     return;
   }
 });
+
+btnPause.addEventListener("click", () => {
+  if (pause === false) {
+    btnPause.innerText = "Reprendre";
+  } else if (pause === true) {
+    btnPause.innerText = "Pause";
+  }
+  pause = !pause;
+});
+
+function afficheTemps(tempsInitial, tempsDeRepos) {
+  affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${
+    tempsInitial % 60 < 10 ? `0${tempsInitial % 60}` : tempsInitial % 60
+  }`;
+  affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${
+    tempsDeRepos % 60 < 10 ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60
+  }`;
+  return;
+}
