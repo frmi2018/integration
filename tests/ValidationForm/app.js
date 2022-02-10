77e2482dc79f13361b8fe1334f87df7bfa3828cf
frmi <frmi2018@gmail.com>
2022-02-10 17:38:41 +0100

    validation mdp

--- tests/ValidationForm/app.js
+++ tests/ValidationForm/app.js
@@ -32,3 +32,57 @@ inpMail.addEventListener("input", (e) => {
     allSpan[1].style.display = "inline";
   }
 });
+
+// Validation et création du MDP
+
+let valeurInp;
+const specialCar = /[^a-zA-Z0-9]/;
+const alphabet = /[a-z]/i;
+const chiffres = /[0-9]/;
+let objValidation = {
+  symbole: 0,
+  lettre: 0,
+  chiffre: 0,
+};
+
+inpMdp.addEventListener("input", (e) => {
+  valeurInp = e.target.value;
+  // Vérification si caractère ajouté
+  if (valeurInp.search(specialCar) !== -1) {
+    objValidation.symbole = 1;
+  }
+  if (valeurInp.search(alphabet) !== -1) {
+    objValidation.lettre = 1;
+  }
+  if (valeurInp.search(chiffres) !== -1) {
+    objValidation.chiffre = 1;
+  }
+  // Vérification si caractère supprimé
+  if ((e.inputType = "deleteContentBackward")) {
+    if (valeurInp.search(specialCar) === -1) {
+      objValidation.symbole = 0;
+    }
+    if (valeurInp.search(alphabet) === -1) {
+      objValidation.lettre = 0;
+    }
+    if (valeurInp.search(chiffres) === -1) {
+      objValidation.chiffre = 0;
+    }
+  }
+  // console.log(objValidation);
+  let testAll = 0;
+  // Tester chaques propriétées de l'objet objValidation
+  for (const property in objValidation) {
+    if (objValidation[property] > 0) {
+      testAll++;
+    }
+  }
+  if (testAll < 3) {
+    allSpan[2].style.display = "inline";
+    allImg[2].style.display = "inline";
+    allImg[2].src = "ressources/error.svg";
+  } else {
+    allSpan[2].style.display = "none";
+    allImg[2].src = "ressources/check.svg";
+  }
+});
