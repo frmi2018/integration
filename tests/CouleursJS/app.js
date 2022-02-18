73c1c20276813d10bbef47678baf93ad479bbbd5
frmi <frmi2018@gmail.com>
2022-02-18 17:46:25 +0100

    random couleurs

--- tests/CouleursJS/app.js
+++ tests/CouleursJS/app.js
@@ -3,6 +3,8 @@ const inputRange = document.querySelector(".inp-range");
 const btns = document.querySelectorAll("button");
 const fond = document.body;
 const containerCouleurs = document.querySelector(".container-couleurs");
+const span = document.querySelector(".span-infos");
+const btnRandom = document.querySelector(".random");
 
 // Démarrage
 let valCouleurs = ["#BA5370", "#F4E2D8"];
@@ -28,13 +30,13 @@ btns.forEach((btn) => {
 });
 
 function rajouteEnleve(e) {
+  span.innerText = "";
   const allInputs = document.querySelectorAll(".inp-couleur");
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   if (e.target.className === "plus") {
     if (allInputs.length > 7) {
       return;
     }
-
     const nvCouleur = document.createElement("input");
     nvCouleur.setAttribute("class", "inp-couleur");
     nvCouleur.setAttribute("data-index", index);
@@ -43,8 +45,27 @@ function rajouteEnleve(e) {
     nvCouleur.style.background = `#${randomColor}`;
     containerCouleurs.appendChild(nvCouleur);
     valCouleurs.push(`#${randomColor.toUpperCase()}`);
-    // Maj du fond
     fond.style.background = `linear-gradient(${inclinaison}deg,${valCouleurs})`;
     index++;
+  } else if (e.target.className === "moins") {
+    if (valCouleurs.length === 2) {
+      span.innerText = "Il faut au moins deux couleurs !";
+    } else {
+      valCouleurs.pop();
+      allInputs[allInputs.length - 1].remove();
+      fond.style.background = `linear-gradient(${inclinaison}deg,${valCouleurs})`;
+    }
   }
 }
+
+// Couleurs aléatoires
+
+btnRandom.addEventListener("click", () => {
+  const inputs = document.querySelectorAll(".inp-couleur");
+  for (let i = 0; i < valCouleurs.length; i++) {
+    valCouleurs[i] = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    inputs[i].value = valCouleurs[i].toUpperCase();
+    inputs[i].style.background = valCouleurs[i];
+    fond.style.background = `linear-gradient(${inclinaison}deg,${valCouleurs})`;
+  }
+});
