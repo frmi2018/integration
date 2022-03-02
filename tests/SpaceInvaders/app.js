d150665a3e51fc99e60e8dd3d9d19cf0c2d0e287
frmi <frmi2018@gmail.com>
2022-03-02 22:38:46 +0100

    HTML/CSS et création grille

---
+++ tests/SpaceInvaders/app.js
@@ -0,0 +1,26 @@
+const container = document.querySelector(".grille");
+let toutesLesDivs;
+let alienInvaders = [];
+
+function creationGrilleEtAliens() {
+  let indexAttr = 0;
+  for (let i = 0; i < 240; i++) {
+    if (indexAttr === 0) {
+      const bloc = document.createElement("div");
+      bloc.setAttribute("data-left", "true");
+      container.append(bloc);
+      indexAttr++;
+    } else if (indexAttr === 19) {
+      const bloc = document.createElement("div");
+      bloc.setAttribute("data-right", "true");
+      container.append(bloc);
+      indexAttr = 0;
+    } else {
+      const bloc = document.createElement("div");
+      container.append(bloc);
+      indexAttr++;
+    }
+  }
+}
+
+creationGrilleEtAliens();
